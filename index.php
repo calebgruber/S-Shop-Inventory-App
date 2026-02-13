f758e80151b7edb5c1b9842e424398fd7e8ca9ed
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2026-02-13 12:46:20 -0500

    Remove Close button from modal footer

--- index.php
+++ index.php
@@ -279,7 +279,6 @@ if ($isDesigner || $isProductionAudio) {
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
